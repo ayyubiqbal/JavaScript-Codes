d69abdf82989688ea45e52f876541517ef58da06
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-02-16 23:19:20 +0600

    prototype

--- Prototype/app.js
+++ Prototype/app.js
@@ -1,6 +1,8 @@
 // console.log('hello world');
 
-let person = {}
+
+// ------------------------------
+/* let person = {}
 
 person.name = 'Iqbal';
 person.age = 25;
@@ -13,4 +15,54 @@ person.sleep = function () {
     console.log(`Iqbal is sleeping`);
 }
 
-console.log(person);
+console.log(person); */
+
+// ------------------------------
+
+/* const personMethods = {
+    eat() {
+        console.log(`Iqbal is eating`);
+    },
+    sleep() {
+        console.log(`Iqbal is sleeping`);
+    },
+    play() {
+        console.log(`Iqbal is playing`);
+    },
+};
+
+
+function Person(name, age) {
+    let person = Object.create(personMethods)
+    // console.log(person);
+
+    person.name = name;
+    person.age = age;
+
+    return person;
+}
+
+const tamim = Person('Tamim', 25);
+tamim.play()
+const iqbal = Person('Iqbal', 23); */
+
+// console.log(tamim);
+
+// ------------------------------
+
+/* const captain = {
+    name: 'Sakib',
+    age: 32,
+    country: 'Bangladesh',
+}
+
+// player child, captain parent
+const player = Object.create(captain)
+
+console.log(player.name); */
+
+// ------------------------------
+
+function test() { }
+
+console.dir(test)
