d4fe58f45a51d7c560ab5dd9dbf5ff709b54f292
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-02-16 22:30:43 +0600

    js prototype

---
+++ Prototype/app.js
@@ -0,0 +1,16 @@
+// console.log('hello world');
+
+let person = {}
+
+person.name = 'Iqbal';
+person.age = 25;
+
+person.eat = function () {
+    console.log(`Iqbal is eating`);
+}
+
+person.sleep = function () {
+    console.log(`Iqbal is sleeping`);
+}
+
+console.log(person);
