2c310af8768a19ecdd8589118efa76226b65741b
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-02-17 14:22:04 +0600

    getter, setter, static method

--- OOP/class/class.js
+++ OOP/class/class.js
@@ -25,11 +25,48 @@ class Developer extends Person { //sub-class
 
 const ayyub = new Developer('Ayyub', 23, 'MERN Stack', 'Bangladesh');
 
-console.log(ayyub);
-ayyub.eat()
-ayyub.play()
+// console.log(ayyub);
+// ayyub.eat()
+// ayyub.play()
 
 const iqbal = new Person('Iqbal', 24)
-console.log(iqbal);
-iqbal.eat()
+// console.log(iqbal);
+// iqbal.eat()
 // iqbal.play()
+
+// --------------------------------------------------
+class User {
+    constructor(name, age) {
+        this.name = name; //property
+        this.age = age;
+    }
+
+    play() { // method
+        console.log(`${this.name} is playing`);
+    }
+
+    static isEqualAge(person1, person2) { // static method
+        if (person1.age < person2.age) console.log(`user 2 is elder`)
+    }
+
+    /* get setName() { //getter
+        return this.name;
+    }
+
+    set setName(name) { //setter
+        this.name = name;
+    } */
+}
+
+let user1 = new User('Ayyub', 22);
+let user2 = new User('Iqbal', 23);
+
+console.log(user1);
+
+User.isEqualAge(user1, user2)
+
+/* 
+console.log(user1.setName);
+user1.setName = 'Ayyub';
+console.log(user1.name);
+ */
