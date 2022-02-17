a9be4c17161a701ff0d41e03fbb2ef23d58064ce
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-02-17 23:14:02 +0600

    switch case

---
+++ logic-condition/switch.js
@@ -0,0 +1,33 @@
+var date = new Date()
+
+// 0 - sunday, 6 - saturday
+
+var today = date.getDay()
+
+// console.log(date, today);
+
+switch (today) {
+    case 0:
+        console.log('Today is Sunday');
+        break;
+    case 1:
+        console.log('Today is Monday');
+        break;
+    case 2:
+        console.log('Today is Tuesday');
+        break;
+    case 3:
+        console.log('Today is Wednesday');
+        break;
+    case 4:
+        console.log('Today is Thursday');
+        break;
+    case 5:
+        console.log('Today is Friday');
+        break;
+    case 6:
+        console.log('Today is Saturday');
+        break;
+
+    default: console.log('Not a valid number');
+}
