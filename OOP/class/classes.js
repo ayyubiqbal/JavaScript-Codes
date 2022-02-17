b0fa17fd252ab5e66237397aa21a3e5e85a04aff
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-02-17 12:23:24 +0600

    prototype inheritance

--- OOP/class/classes.js
+++ OOP/class/classes.js
@@ -2,10 +2,18 @@ function Person(name, age) {
     this.name = name;
     this.age = age;
 
-    /*     this.eat = function () {
-            console.log(`${this.name} is eating`);
-        }
-    */
+    // this.eat = function () {
+    //     console.log(`${this.name} is eating`);
+    // }
+
+}
+
+function Developer(name, age, stack, country) {
+    Person.call(this)
+    this.name = name;
+    this.age = age;
+    this.stack = stack;
+    this.country = country;
 }
 
 Person.prototype = {
@@ -14,15 +22,20 @@ Person.prototype = {
     }
 }
 
-const ayyub = new Person('Ayyub', 25);
-// ayyub.eat()
+Developer.prototype = Object.create(Person.prototype);
+Developer.prototype.constructor = Developer;
+
+const ayyub = new Developer('Ayyub', 23, 'MERN Stack', 'Bangladesh');
+ayyub.eat()
 const iqbal = new Person('Iqbal', 23);
 
 console.log(ayyub);
 console.log(iqbal);
+
 // Person {name: 'Iqbal', age: 23, eat: ƒ}
 
 
+// -----------------------------
 
 /* const obj = {
     name: 'iqbal',
@@ -30,3 +43,19 @@ console.log(iqbal);
 }
 
 console.log(typeof obj, obj); */
+
+// -----------------------------
+
+/* const f = function Person() { };
+// console.dir(f);
+
+Object.prototype.iqbal = function () {
+    console.log('Hi, I\'m Iqbal from prototype');
+}
+
+const p = {};
+p.iqbal()
+
+console.dir(Object); */
+
+// -----------------------------
