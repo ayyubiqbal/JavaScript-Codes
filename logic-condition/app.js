7b5039da7385a9e0ca6c0d8280b580f2e2326558
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-02-18 15:35:10 +0600

    ternary, logical-op shorthand

---
+++ logic-condition/app.js
@@ -0,0 +1,27 @@
+var n = 10;
+
+var str = ''
+
+/* if (n % 2 === 0) {
+    str = 'Even'
+}
+else {
+    str = 'Odd'
+}
+
+console.log(str); */
+
+var result = n % 2 === 0 ? 'Even Number' : 'Odd Number';
+// console.log(result);
+
+//-----------------------------------
+
+var name = ''
+
+var fullName = name || 'Ayyub Iqbal';
+// console.log(fullName);
+
+//-----------------------------------
+
+var isTrue = true;
+// isTrue && console.log('Everything is Ok');
