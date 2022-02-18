4a72f321ed0a182ee46d51a374e59346459d0943
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-02-18 20:30:58 +0600

    while loop

--- logic-condition/app.js
+++ logic-condition/app.js
@@ -54,5 +54,25 @@ for (let i = 1; i <= 10; i++) {
     sum += i;
 }
 
-console.log(`even sum is = ${evenSum}`);
-console.log(`sum is = ${sum}`);
+// console.log(`even sum is = ${evenSum}`);
+// console.log(`sum is = ${sum}`);
+
+
+// var i = 1;
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+var isRunning = true;
+
+while (isRunning) {
+    random = Math.round(Math.random() * 10)
+    if (random === 10) {
+        console.log('Hello, I\'m number 10');
+        isRunning = false;
+    } else {
+        console.log(`hello, I'm number ${random}`);
+    }
+
+}
