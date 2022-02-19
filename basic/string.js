bd0ce7f2686d49b641efbb31a240bda4850b8111
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-02-19 11:26:58 +0600

    String comparison

--- basic/string.js
+++ basic/string.js
@@ -8,8 +8,25 @@ var str6 = String(3.134)
 var num = 10;
 var str7 = num.toString()
 
-console.log(str1, str2, str3, str4, str5, str6, str7)
+// console.log(str1, str2, str3, str4, str5, str6, str7)
 
 // Escape Notation
-console.log('Hello,\nI\'m Ayyub \\ Iqbal');
+// console.log('Hello,\nI\'m Ayyub \\ Iqbal');
 // \r, \v, \b, \f
+
+
+// String comparison
+var a = 'abc';
+var b = 'cba';
+
+// console.log(a < b);
+
+// a = 0 - z = 26;
+// A = 0 - Z = 26;
+// but, but small letter always greater than capital letter
+// console.log('z' > 'a');
+// console.log('Z' > 'A');
+// console.log('a' > 'Z');
+// console.log('Z' > 'a');
+
+console.log('001' == 1);
