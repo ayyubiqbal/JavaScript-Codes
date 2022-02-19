dfa27aba979d6ddecc9fddcc42d1bb6affb68ca3
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-02-19 09:26:42 +0600

    string, escape notation

--- basic/string.js
+++ basic/string.js
@@ -5,5 +5,11 @@ var str3 = `Hello World Bangladesh`
 var str4 = String('Hello World')
 var str5 = String(150)
 var str6 = String(3.134)
+var num = 10;
+var str7 = num.toString()
 
-console.log(str1, str2, str3, str4, str5, str6)
+console.log(str1, str2, str3, str4, str5, str6, str7)
+
+// Escape Notation
+console.log('Hello,\nI\'m Ayyub \\ Iqbal');
+// \r, \v, \b, \f
