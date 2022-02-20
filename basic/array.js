b7730cacac3a0ed1411ba66f01d186f82263ff5b
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-02-20 08:58:37 +0600

    array traverse

--- basic/array.js
+++ basic/array.js
@@ -9,9 +9,9 @@ arr.pop()
 arr.splice(2, 1, 333)
 // arr[10] = 100;
 // console.log(arr[6]);
-console.log(arr);
+/* console.log(arr);
 console.log(arr.length);
-
+ */
 var arr2 = Array(10, 12, 14)
 // console.log(arr2);
 
@@ -38,3 +38,50 @@ for (i = 0; i < arr4.length; i++) {
     sum = sum + arr4[i];
 }
 // console.log(sum);
+
+// ----------------------------------------
+
+var arr5 = [23, 56, 70, 10, 25]
+var find = 20;
+var isFound = false;
+
+for (var i = 0; i < arr5.length; i++) {
+    if (find === arr5[i]) {
+        console.log('Data is found in index', i);
+        isFound = true;
+        break;
+    }
+
+}
+if (!isFound) {
+    // console.log(!isFound);
+    // console.log('Data is not found');
+}
+
+// ----------------------------------------
+
+var arr6 = [
+    [12, 34, 56, 78],
+    [21, 43, 65, 87],
+    [31, 42, 75, 97]
+]
+
+for (var i = 0; i < arr6.length; i++) {
+    for (var j = 0; j < arr6[i].length; j++) {
+        // console.log(arr6[i][j]);
+    }
+}
+
+// ----------------------------------------
+
+
+var arr7 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+/* for (var i = 0; i < (arr7.length / 2); i++) {
+    var temp = arr7[i]
+    arr7[i] = arr7[arr7.length - 1 - i]
+    arr7[arr7.length - 1 - i] = temp;
+}
+console.log(arr7); */
+
+console.log(arr7.reverse()); 
