a570d7f8b763a8f4a79ce1e7294258a8f9b32ed7
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-02-21 21:35:55 +0600

    params, arguments

--- basic/function.js
+++ basic/function.js
@@ -1,3 +1,4 @@
+// input, output, processing
 function myFunction() {
     console.log('Hello, function');
 }
@@ -27,3 +28,57 @@ sum()
 sub()
 
 console.log(sum.name);
+
+console.log('-------------------------');
+
+
+// parameters - arguments
+
+function add(a, b) {
+    var result = a + b;
+    console.log(result);
+}
+
+add(10, 20)
+add(2010, 20050)
+console.log('-------------------------');
+
+
+// ------------------------------------
+var arr1 = [1, 2, 3]
+var arr2 = [1, 3, 3]
+var arr3 = [1, 2, 5]
+
+var sum1 = 0;
+for (var i = 0; i < arr1.length; i++) {
+    sum1 += arr1[i]
+}
+console.log(sum1);
+
+var sum2 = 0;
+for (var i = 0; i < arr2.length; i++) {
+    sum2 += arr2[i]
+}
+console.log(sum2);
+
+var sum3 = 0;
+for (var i = 0; i < arr3.length; i++) {
+    sum3 += arr3[i]
+}
+console.log(sum3);
+
+console.log('-------------------------');
+
+// ------------------------------
+
+function arraySum(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    console.log(sum);
+}
+arraySum(arr1)
+arraySum(arr2)
+arraySum(arr3)
+arraySum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
