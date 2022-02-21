bf329f1aa509926dec071d45d72f064e8d1c3e18
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-02-21 21:04:38 +0600

    basic function

---
+++ basic/function.js
@@ -0,0 +1,29 @@
+function myFunction() {
+    console.log('Hello, function');
+}
+
+function sum() {
+    var a = 10;
+    var b = 20;
+    console.log(a + b);
+}
+
+function sub() {
+    var a = 20;
+    var b = 10;
+    console.log(a - b);
+}
+
+// console.log(a, b);
+
+// myFunction()
+// myFunction()
+
+/* for (var i = 1; i <= 10; i++) {
+    myFunction()
+} */
+
+sum()
+sub()
+
+console.log(sum.name);
