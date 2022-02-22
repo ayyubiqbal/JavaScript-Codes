8ef96866a3914ac9bd4c7c3a64154ea89771ecc6
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-02-22 22:13:09 +0600

    pure function, side effects

---
+++ functional-programming/pure.js
@@ -0,0 +1,35 @@
+// Pure Function
+/* 
+`It returns the same result if given the same arguments`
+`It does not cause any observable side effects`
+*/
+
+function sqr(n) {
+    return n * n;
+}
+console.log(sqr(2));
+console.log(sqr(2));
+console.log(sqr(2));
+
+
+var n = 10;
+function MyFunc() {
+    n = 100;
+}
+MyFunc()
+console.log(n);
+
+var point = {
+    x: 10,
+    y: 20,
+}
+
+function printPoint(point) {
+    point.x = 100;
+    point.y = 200;
+    console.log('inner', point);
+}
+
+console.log('outer, before call', point);
+printPoint(point)
+console.log('outer, after call', point);
