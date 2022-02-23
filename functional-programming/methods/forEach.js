503968e1489a1ed3d1f47d47bdbba0486ae91e3c
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-02-23 17:38:57 +0600

    how to forEach method work

---
+++ functional-programming/methods/forEach.js
@@ -0,0 +1,27 @@
+var arr = [1, 2, 3, 4, 5]
+
+/* var sum = 0;
+arr.forEach(function (value, index, array) {
+    sum += value;
+    console.log(value, index, array);
+})
+
+console.log(sum); */
+
+function forEach(arr, cb) {
+    for (var i = 0; i < arr.length; i++) {
+        cb(arr[i], i, arr)
+    }
+}
+
+var add = 0;
+forEach(arr, function (value, index, array) {
+    add += value;
+    console.log(value, index, array);
+});
+console.log(add);
+
+forEach(arr, function (value, index, array) {
+    array[index] = value * 5;
+})
+console.log(arr);
