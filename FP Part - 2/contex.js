82061174d8df20a82f10a7b781489a1f3164c8e9
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-02-27 21:49:14 +0600

    Execution Context

---
+++ FP Part - 2/contex.js
@@ -0,0 +1,29 @@
+// Execution Context
+function a() {
+    b()
+    console.log('I am function A');
+}
+
+function b() {
+    d()
+    console.log('I am function B');
+}
+
+function c() {
+    console.log('I am function C');
+}
+function d() {
+    c()
+    console.log('I am function D');
+}
+
+var x = 100;
+a()
+console.log('I am Global Context');
+
+
+// c()
+// d()
+// b()
+// a()
+// execution of global context
