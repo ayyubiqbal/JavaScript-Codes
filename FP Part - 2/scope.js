3a7a127f5d952f5d0a6547946516b14a279bd843
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-02-28 14:56:52 +0600

    scope chain

--- FP Part - 2/scope.js
+++ FP Part - 2/scope.js
@@ -3,11 +3,11 @@
 
     function myFunc() {
         // var x = 200;
-        console.log(x);
+        // console.log(x);
 
         function myNestedFunc() {
             var y = 30;
-            console.log(x);
+            // console.log(x);
         }
 
         myNestedFunc()
@@ -18,3 +18,37 @@
     // console.log(x);
 
 }
+
+
+// scope chain
+
+var a = 10;
+
+function A() {
+    var b = 20;
+
+    function B() {
+        var c = 30;
+        console.log(c);
+    }
+
+    function C() {
+        var d = 40;
+        console.log(d);
+    }
+
+    console.log(b);
+
+    D(b)
+    B()
+    C()
+}
+
+function D(n) {
+    return n + a
+}
+
+// A -> a,b, B(),C(),D()
+// B -> a,b,c, B(),C(),D()
+// C -> a,b,d, B(),C(),D()
+// D -> a,n, A()
