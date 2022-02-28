120cc06982148084215b0c5d2baf9cfc79a397bd
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-02-28 11:36:27 +0600

    function exp

--- FP Part - 2/function.js
+++ FP Part - 2/function.js
@@ -4,9 +4,16 @@ function myFunc() {
     console.log('I am a Function');
 }
 
-myFunc()
+// myNewFunc()
+
+var myNewFunc = function () {
+    console.log('I am a new Function');
+}
+
+myNewFunc()
 
 // Creational Phase
 // myFunc = function ref
+// myNewFunc = undefined
 
 // Executional Phase
