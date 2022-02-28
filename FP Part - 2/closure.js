41c93d6d832be5e483629779aecc07f882b42c34
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-02-28 15:30:04 +0600

    closure, lexical scope

---
+++ FP Part - 2/closure.js
@@ -0,0 +1,30 @@
+// Closure is when a function is able to remember and access it's lexical scope even when that function executing outside it's lexical scope
+
+// 1. when a function is able to remember and access it's lexical scope
+// 2. when that function executing outside it's lexical scope
+
+
+/* 
+function myFunc() {
+    var msg = 'Lexical Scope and Closure';
+
+    function sayMsg() {
+        console.log(msg);
+    }
+
+    sayMsg()
+}
+myFunc()
+ */
+
+function myFunc() {
+    var msg = 'Lexical Scope and Closure';
+
+    return function () {
+        console.log(msg);
+    }
+
+
+}
+var sayMsg = myFunc()
+sayMsg()
