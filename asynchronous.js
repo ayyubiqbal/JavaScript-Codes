fb0fc286ee68fab8d9fe1526202519096c31bfbd
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-03-01 15:51:08 +0600

    pyramidOfDoom

--- asynchronous.js
+++ asynchronous.js
@@ -75,3 +75,38 @@ second(third) */
 
 
 // ---------------------------
+
+function pyramidOfDoom() {
+    setTimeout(() => {
+        console.log(1);
+        setTimeout(() => {
+            console.log(2);
+            setTimeout(() => {
+                console.log(3);
+                setTimeout(() => {
+                    console.log(4);
+                    setTimeout(() => {
+                        console.log(5);
+                        setTimeout(() => {
+                            console.log(6);
+                            setTimeout(() => {
+                                console.log(7);
+                                setTimeout(() => {
+                                    console.log(8);
+                                    setTimeout(() => {
+                                        console.log(9);
+                                        setTimeout(() => {
+                                            console.log(10);
+                                        }, 10000)
+                                    }, 9000)
+                                }, 8000)
+                            }, 7000)
+                        }, 6000)
+                    }, 5000)
+                }, 4000)
+            }, 3000)
+        }, 2000)
+    }, 1000)
+}
+
+pyramidOfDoom()
